6b66ecfffe04e9947cc0e3ad285f9c59897f3445
zoeGuava <zoeguava@gmail.com>
2024-09-23 14:24:16 +0800

    feat: initial project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gpt-line-bot
-# gpt-line-bot
+ build this project from [用 Node.js 建立你的第一個 LINE Bot 聊天機器人以 OpenAI GPT-3 與 GPT-3.5 為例
+](https://israynotarray.com/nodejs/20221210/1224824056/#%E5%89%8D%E7%BD%AE%E5%8B%95%E4%BD%9C)
---
+++ app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+// create LINE SDK config from env variables
+const config = {
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create an echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [echo],
+  });
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
